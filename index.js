0e4b5fc05cb688207a7584e732e0c90d966643f0
Cameron Bisca <cjbisca@gmail.com>
2021-01-11 13:29:28 -0500

    commit tree 4a48fb2013a47f59ab4b550eec248438fd1ccc4e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name);
+}
+
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name);
+}
